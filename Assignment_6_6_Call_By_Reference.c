201d2cb9354c85030f34ae3edbccc0f69f9d7fa9
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-14 19:55:39 +0530

    Add files via upload

---
+++ Assignment_6_6_Call_By_Reference.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void disp(int *num)
+{
+    printf("%d  ", *num);
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    for (int i = 0; i < 9; i++)
+    {
+        disp(&arr[i]);
+    }
+    return 0;
+}
